a97b18f5ed0a3bc2dd095a06670fb0e562097781
HuZheting <huzheting@yeah.net>
2018-10-08 20:49:20 +0800

    MaximumSubarray_53

---
+++ array/MaximumSubarray_53.java
@@ -0,0 +1,12 @@
+
+class MaximumSubarray_53{
+
+	public int maxSubArray(int[] nums) {
+		int curMax = 0, max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; ++i){
+        	curMax = Math.max(curMax + nums[i], nums[i]);
+        	max = (max, curMax);
+        }
+
+    }
+}
