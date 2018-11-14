8e5e8a3e1372f038a274bc1d4d6fbf9ba156c4cc
zhetinghu <huzheting@yeah.net>
2018-11-14 15:38:33 +0800

    modify Quicksort

--- sort/Quicksort.java
+++ sort/Quicksort.java
@@ -11,18 +11,13 @@ class Quicksort{
             while(left < right){
             	    while(nums[right] >= key && left < right)
             	        --right;
-            	    if(nums[right] < key){
-            	    	nums[left] = nums[right];
-            	    	nums[right] = key;
-            	    }
-            	    while(nums[left] < key && left < right)
-            	    	++left;
+            	    nums[left] = nums[right];
             	    
-            	    if(nums[left] >= key){
-            	    	nums[right] = nums[left];
-            	    	nums[left] = key;
-            	    }
+            	    while(nums[left] < key && left < right)
+            	        ++left;
+            	    nums[right] = nums[left];
             }
+            nums[left] = key;
             quicksort(nums, start, left - 1);
             quicksort(nums, left + 1, end);
         }
@@ -31,12 +26,12 @@ class Quicksort{
     public static void test(int n, int rangeL, int rangeR){
         int[] nums= SortTestHelper.generateRandomArray(n, rangeL, rangeR);
         Quicksort.sort(nums);
-        assert SortTestHelper.isSorted(nums);
+        System.out.println( SortTestHelper.isSorted(nums));
     }
 
     public static void main(String[] args){
         for(int i = 0; i < args.length; ++i){
-        	Quicksort.test(Integer.parseInt(args[i]), -1000, 1000);
+            Quicksort.test(Integer.parseInt(args[i]), -1000, 1000);
         }
     }
 }
